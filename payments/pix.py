a451160903016a509c55f8d200e048807307c2c7
Igor <igor.vguerra@hotmail.com>
2025-07-14 20:20:37 +0100

    integratin the bank info to the creation of the payment and implementing the qr code visualization

--- payments/pix.py
+++ payments/pix.py
@@ -7,12 +7,12 @@ class Pix:
 
     def create_payment(self):
         
-        bank_payment_id = uuid.uuid4()
+        bank_payment_id = str(uuid.uuid4())
 
         hash_payment = f"hash_payment_{bank_payment_id}"
 
         img = qrcode.make(hash_payment)
         img.save(f"static/img/qr_code_payment_{bank_payment_id}.png")
 
-        return {"bak_payment_id": bank_payment_id,
+        return {"bank_payment_id": bank_payment_id,
                 "qr_code_path": f"qr_code_payment_{bank_payment_id}"}
