118214cabe0ebeabc81da24d26ee04a3628efbb9
Igor <igor.vguerra@hotmail.com>
2025-07-17 13:27:54 +0100

    using websocket to change the page for the client

--- app.py
+++ app.py
@@ -55,12 +55,14 @@ def pix_confirmation():
     
     payment.paid = True
     db.session.commit()
+    socketio.emit(f'payment-confirmed-{payment.id}')
     return jsonify({"message" : "Payment was confirmed."})
 
 @app.route('/payments/pix/<int:payment_id>', methods=['GET'])
 def payment_pix_page(payment_id):
     payment = Payment.query.get(payment_id)
-
+    if payment.paid:
+        return render_template('confirmed_payment.html', payment_id=payment.id, value=payment.value, qr_code=payment.qr_code)
     return render_template('payment.html', payment_id=payment.id, value=payment.value, host="http://127.0.0.1:5000", qr_code=payment.qr_code)
 
 
